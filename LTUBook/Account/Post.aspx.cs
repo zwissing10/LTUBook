14c777bd5395cdf03ae13facceaab383d010cb9a
Zach Wissing <zwissing@imagesoftinc.com>
2019-04-30 16:59:04 -0400

    Fixing DLL, POST page is BROKEN

--- LTUBook/Account/Post.aspx.cs
+++ LTUBook/Account/Post.aspx.cs
@@ -11,13 +11,15 @@ namespace LTUBook.Account
 {
     public partial class Post : System.Web.UI.Page
     {
+        string currSelectedUser;
+        List<string> userIdList = new List<string>();
         SqlConnection db = new SqlConnection("Data Source = (LocalDb)\\MSSQLLocalDB;Initial Catalog=aspnet-LTUBook-20190228033437;Integrated Security=True");
+
         protected void Page_Load(object sender, EventArgs e)
         {
             db.Open();
 
-            string currUserId = User.Identity.GetUserId();
-            List<string> userIdList = new List<string>(); 
+            string currUserId = User.Identity.GetUserId(); 
 
             SqlCommand cmd = new SqlCommand("SELECT [FriendId] FROM Friends WHERE UserId = '" + currUserId + "';", db);
             SqlDataReader dr = cmd.ExecuteReader();
@@ -33,11 +35,18 @@ namespace LTUBook.Account
             {
                 UserList.Items.Add(dr.GetValue(0).ToString());
             }
+            dr.Close();
+        }
+
+        protected void UserList_SelectionChanged(object sender, EventArgs e)
+        {
+            //currSelectedUser
         }
 
         protected void post(object sender, EventArgs e)
         {
-
+            string currUserId = User.Identity.GetUserId();
+            //string recUserId = 
         }
     }
 }
